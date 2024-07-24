dda7684141bb91b5bded941b2ca9240097e47158
sheraz4196 <sheraztoor321@gmail.com>
2024-07-24 16:08:18 +0500

    feat: subscribe section UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Subscribe from "@/components/general/subscribe";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <Subscribe />
+    </>
   );
 }
---
+++ components/general/subscribe.tsx
@@ -0,0 +1,12 @@
+export default function Subscribe() {
+  return (
+    <section className="px-16 flex items-center justify-center bg-gradient-to-r from-primary-dark to-primary">
+      <div className="py-12 flex items-center justify-between w-full">
+        <h2 className="text-muted text-2xl font-bold">
+          Signup our newsletter to get update information, news, insight or
+          promotions.
+        </h2>
+      </div>
+    </section>
+  );
+}
