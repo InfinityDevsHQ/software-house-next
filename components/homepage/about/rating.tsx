ab2df2fe2010063c2f6d40fcd92819baf7d46d0d
sheraz4196 <sheraztoor321@gmail.com>
2024-07-25 15:15:54 +0500

    feat: rating component

---
+++ components/homepage/about/rating.tsx
@@ -0,0 +1,25 @@
+import FiveStars from "./five-stars";
+
+export default function Rating() {
+  return (
+    <div className="border-t-2 border-muted flex gap-5">
+      <div className="py-5 flex flex-col gap-2.5  items-center justify-center text-center">
+        <p className="text-4xl font-bold text-primary-medium">4.7+</p>
+        <FiveStars />
+        <p className="font-bold text-accent-dark">Review Customer</p>
+        <p className="text-accent-foreground text-xs">
+          Erat mauris posuere dui sed faucibus
+        </p>
+      </div>
+      <span className="border-2 border-muted" />
+      <div className="py-5 flex flex-col gap-2.5  items-center justify-center text-center">
+        <p className="text-4xl font-bold text-primary-medium">B+</p>
+        <FiveStars />
+        <p className="font-bold text-accent-dark">Business Class</p>
+        <p className="text-accent-foreground text-xs">
+          Erat mauris posuere dui sed faucibus
+        </p>
+      </div>
+    </div>
+  );
+}
