4fd3a95b81920dac92b38b53b7e49d551f2b1374
sheraz4196 <sheraztoor321@gmail.com>
2024-07-25 14:10:40 +0500

    feat: solution card component

---
+++ components/homepage/services/solution-card.tsx
@@ -0,0 +1,24 @@
+import { ReactElement } from "react";
+
+export type SolutionCardProps = {
+  heading: string;
+  description: string;
+  icon: ReactElement;
+};
+export default function SolutionCard({
+  heading,
+  icon,
+  description,
+}: SolutionCardProps) {
+  return (
+    <article className="p-5 shadows flex items-start gap-2.5">
+      <span className="h-7 w-7">{icon}</span>
+      <div className="flex flex-col gap-2.5">
+        <h4 className="font-medium text-accent-dark text-base leading-5">
+          {heading}
+        </h4>
+        <p className="text-xs">{description}</p>
+      </div>
+    </article>
+  );
+}
