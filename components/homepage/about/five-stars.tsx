abb1d5e26d48c7b4bb0ede8dd0b6a5397f5fdd5a
sheraz4196 <sheraztoor321@gmail.com>
2024-07-25 15:15:40 +0500

    feat: five star components

---
+++ components/homepage/about/five-stars.tsx
@@ -0,0 +1,13 @@
+import { Star } from "lucide-react";
+
+export default function FiveStars() {
+  return (
+    <div className="flex items-center gap-1.5">
+      <Star className="h-5 w-5" color="#F59E0B" fill="#F59E0B" />
+      <Star className="h-5 w-5" color="#F59E0B" fill="#F59E0B" />
+      <Star className="h-5 w-5" color="#F59E0B" fill="#F59E0B" />
+      <Star className="h-5 w-5" color="#F59E0B" fill="#F59E0B" />
+      <Star className="h-5 w-5" color="#F59E0B" fill="#F59E0B" />
+    </div>
+  );
+}
