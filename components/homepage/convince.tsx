37c32894706248a00a089ad92ad03b7f223111c9
sheraz4196 <sheraztoor321@gmail.com>
2024-07-25 12:54:54 +0500

    feat: convince section

--- components/homepage/convince.tsx
+++ components/homepage/convince.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import ListSvg from "../svgs/list-svg";
+import ArrowPointedSvg from "../svgs/arrow-pointed-svg";
 
 export default function Convince() {
   return (
@@ -10,18 +12,47 @@ export default function Convince() {
             alt="Convince Image"
             width={575}
             height={500}
+            className="h-full w-full object-cover"
           />
         </div>
         <div className="contact-hero text-accent p-12 flex flex-col gap-5">
           <h4 className="text-3xl font-bold">
             From Strategy to delivery, on time every time.
           </h4>
-          <p>
+          <p className="text-xs">
             Lorem ipsum dolor sit amet consectetur. Nibh lectus diam egestas
             etiam. Erat mauris posuere dui sed faucibus placerat euismod.
             Blandit facilisis hac leo morbi. Nibh erat hac nibh nibh faucibus
             imperdiet in scelerisque.
           </p>
+          <ol className="max-w-96 p-5 flex flex-col gap-5">
+            <li className="flex gap-2.5 text-accent-light">
+              <span>
+                <ListSvg />
+              </span>
+              <div className="flex flex-col gap-2.5">
+                <h4 className="text-base font-medium">Business Strategy</h4>
+                <p className="text-xs">
+                  Lorem ipsum dolor sit amet consectetur. Nibh lectus diam
+                  egestas etiam. Erat mauris posuere dui sed faucibus placerat
+                  euismod.
+                </p>
+              </div>
+            </li>
+            <li className="flex gap-2.5 text-accent-light">
+              <span>
+                <ArrowPointedSvg />
+              </span>
+              <div className="flex flex-col gap-2.5">
+                <h4 className="text-base font-medium">Business Strategy</h4>
+                <p className="text-xs">
+                  Lorem ipsum dolor sit amet consectetur. Nibh lectus diam
+                  egestas etiam. Erat mauris posuere dui sed faucibus placerat
+                  euismod.
+                </p>
+              </div>
+            </li>
+          </ol>
         </div>
       </div>
     </section>
