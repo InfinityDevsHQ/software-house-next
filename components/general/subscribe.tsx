95a55ed18c1aa78c16c8a807b7e3e57c9e22ada1
Danish <danishasifdev@gmail.com>
2024-07-26 19:44:04 +0500

    fix: fix subscribe section

--- components/general/subscribe.tsx
+++ components/general/subscribe.tsx
@@ -2,9 +2,9 @@ import SubscribeForm from "../forms/subscribe-form";
 
 export default function Subscribe() {
   return (
-    <section className="px-16 flex items-center justify-center bg-gradient-to-r from-primary-dark to-primary">
-      <div className="py-12 lg:grid grid-cols-2 items-center justify-between gap-8 w-full">
-        <h2 className="text-muted text-2xl font-bold mb-12 lg:mb-0">
+    <section className="px-4 sm:px-16 flex items-center justify-center bg-gradient-to-r from-primary-dark to-primary">
+      <div className="py-12 px-4 lg:grid grid-cols-2 items-center justify-between gap-8 w-full">
+        <h2 className="text-muted text-2xl font-bold mb-6 sm:mb-12 lg:mb-0 text-center sm:text-start">
           Signup our newsletter to get update information, news, insight or
           promotions.
         </h2>
