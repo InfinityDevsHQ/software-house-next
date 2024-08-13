863e7f8547a8d166c623d928eed5fd9f7e264de4
sheraz4196 <sheraztoor321@gmail.com>
2024-08-13 14:44:11 +0500

    fix: container

--- components/homepage/about.tsx
+++ components/homepage/about.tsx
@@ -3,8 +3,8 @@ import Rating from "./about/rating";
 
 export default function About() {
   return (
-    <section className="px-4 md:px-12 py-12 lg:py-24 lg:h-[500px]">
-      <div className="flex flex-col lg:flex-row gap-4 md:gap-6 lg:gap-12 justify-between">
+    <section className="px-4 md:px-12 py-12 lg:py-24">
+      <div className="flex flex-col lg:flex-row gap-4 md:gap-6 lg:gap-12 justify-between lg:h-[500px]">
         <div className="md:py-6 lg:py-16 flex flex-col gap-5 flex-1 order-2 lg:order-1">
           <header className="flex gap-5 justify-between">
             <div className="flex flex-1 flex-col gap-5 w-full">
